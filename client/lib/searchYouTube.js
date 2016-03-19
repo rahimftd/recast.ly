df8461a6138f455c58dbfccbc74ae9479323a7f5
rahimftd <rahim.dharssi@gmail.com>
2016-03-18 19:18:03 -0700

    Create api request function

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var query = options.query;
+  var max = options.max;
+  var authKey = options.authKey;
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      q: query,
+      maxResults: max,
+      part: 'snippet',
+      key: authKey
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
